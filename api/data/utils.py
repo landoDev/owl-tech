22e5d990a4809a0c858d4d1fe38bc54911c59bcd
landoDev <landonlewisturner@gmail.com>
2023-11-30 17:03:46 -0500

    build out util for cumulative returns wip

--- api/data/utils.py
+++ api/data/utils.py
@@ -1,17 +1,20 @@
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 import csv
+
+import numpy as np
+import pandas as pd
 from pydantic import BaseModel
 
 
-def read_csv_data() -> dict[str, dict[str, str]]:
+def read_csv_data(filepath: str = 'stock-data.csv') -> dict[str, dict[str, str]]:
     stock_data = {}
-    with open('data/stock-data.csv', 'r') as csvdata:
+    with open(filepath, 'r') as csvdata:
         reader = csv.DictReader(csvdata)
         for row in reader:
             # strip whitespace and replace spaces for use as id's in api path parameters in `main.py`
             stock_id = row['name'].strip().replace(" ", "")
             stock_price = {
-                "date": datetime.strptime(row['asof'], '%Y-%m-%d').date(),
+                "date": row['asof'],
                 "volume": row['volume'],
                 "close_usd": row['close_usd']
             }
@@ -22,3 +25,61 @@ def read_csv_data() -> dict[str, dict[str, str]]:
                 stock_data[stock_id]["stock_prices"].append(stock_price)
     return stock_data
 
+def handle_cumulative_returns(name: str, date_from: str, date_to:str):
+    # TODO: consider breaking me out into smaller private functions and renaming this to `handle_cumulative_returns`
+    ## PREPARE DATA ## <= would return a df if it's own fn
+    # read data in
+    df = pd.read_csv('stock-data.csv')
+    df = df[['asof', 'name', 'close_usd']]
+    df.columns = ['date', 'name', 'price']
+    # our endpoint needs the asset names to be stripped and have no spaces. applying lambda so we can filter later
+    df['name'].apply(lambda x: x.strip().replace(" ", ""))
+    df = df[(df['name'] == name)]
+    # only include data of the stock name and the daterange from the input
+    df = df[(df['date'] > date_from) & (df['date'] < date_to)]
+    # pivot the data to make the stock name the column
+    df1 = df.pivot_table(index=['date'], columns='name', values=['price'])
+    df1.columns = [col[1] for col in df1.columns.values]
+
+    # drop any dates with no data
+    df1 = df1.dropna()
+    if df1.empty:
+        print("No data in daterange") 
+        # TODO: turn me into a raise for endpoint
+        return
+
+    ## CALCULATE DAILY RETURNS ##
+    df_daily_returns = df1.pct_change()
+    # skip first row with NA
+    df_daily_returns = df_daily_returns[1:]
+    # print(df_daily_returns.to_dict())
+    df_cumulative_returns = (1 + df_daily_returns).cumprod() - 1
+    df_cumulative_returns = df_cumulative_returns.reset_index()
+    print(df_cumulative_returns) # RETURN ME
+    entire_period_return = df_cumulative_returns.iloc[:, 1:].tail(1)
+    print(entire_period_return * 100) # AND RETURN ME
+
+def _prepare_data_for_returns(name: str, date_from: str, date_to:str) -> pd.DataFrame:
+    df = pd.read_csv('stock-data.csv')
+    df = df[['asof', 'name', 'close_usd']]
+    df.columns = ['date', 'name', 'price']
+    # our endpoint needs the asset names to be stripped and have no spaces. applying lambda so we can filter later
+    df['name'].apply(lambda x: x.strip().replace(" ", ""))
+    df = df[(df['name'] == name)]
+    # only include data of the stock name and the daterange from the input
+    df = df[(df['date'] > date_from) & (df['date'] < date_to)]
+    # pivot the data to make the stock name the column
+    df1 = df.pivot_table(index=['date'], columns='name', values=['price'])
+    df1.columns = [col[1] for col in df1.columns.values]
+
+    # drop any dates with no data
+    df1 = df1.dropna()
+    if df1.empty:
+        print("No data in daterange") 
+        # TODO: turn me into a raise for endpoint
+        return
+
+def _calculate_cumulative_returns(dataframe: pd.DataFrame):
+    pass
+
+handle_cumulative_returns('Apple',  '2013-01-01', '2013-02-01')
--- api/main.py
+++ api/main.py
@@ -1,11 +1,14 @@
 from datetime import datetime, date
-from fastapi import FastAPI
-from pydantic import BaseModel, Field
-from data.utils import read_csv_data
 from typing import Dict, List, TypedDict
 
+from fastapi import FastAPI
+import pandas as pd
+from pydantic import BaseModel, Field
+
+from data.utils import read_csv_data
+
 app = FastAPI()
-app_data = read_csv_data()
+app_data = read_csv_data(filepath='data/stock-data.csv')
 
 ## RETURN TYPES ##
 # NOTE this mirrors how the csv is consumed and organized and is not a perfect representation of what the db models would look like
@@ -38,11 +41,13 @@ async def retrieve_stock(name: str) -> Stock:
     
 @app.get("/stocks/return/{name}")
 ## rely on query params? <- rely on query params for the date range
-async def calculate_return(name: str, date_start: str, date_end: str):
+async def calculate_return(name: str, date_start: str = '', date_end: str = ''): # dont leave defaults, just debugging
     """ Calculate cumulative returns between two dates. """
     asset_prices: list = app_data[name]["stock_prices"]
+    # TODO: UTIL ME
     # filter list of asset_prices to just the daterange
     # make sure it's in order
-    # double check what a cumulative return is
-    # take the two dates on the edge of each range and do the math
-    return "under construction"
+    # find cumulative return formula
+    df = pd.DataFrame(app_data[name]["stock_prices"])
+    # ((df + 1).cumprod()-1).iloc[-1]
+    return df + 2
