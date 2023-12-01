48c51bd407bbcee08b5c311d8cb290f19ad476e2
landoDev <landonlewisturner@gmail.com>
2023-12-01 15:21:52 -0500

    install mui, mui chart and pseudocode high level plan for FE

--- ui/web/src/App.tsx
+++ ui/web/src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { LineChart } from '@mui/x-charts/LineChart';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Future home to all the stock data. The price series should be a graph for each asset in the stock data.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>OWL Stock Prices</h1>
       </header>
+      <div className='App-container'>
+        {/* this page is the home page */}
+        {/* call the all stocks endpoint on render and construct a line chart next to each asset's name  */}
+        {/* The asset name should be big and clickable */}
+        {/* an SPA page opens up with their Line chart, and the total cumulative returns and a table of the daily returns for their lifespan */}
+        {/* above this table is a date filter that the user can use to filter the date range and see the total cumulative returns and daily returns for that period */}
+        <p>Under Construction</p>
+      </div>
     </div>
   );
 }
