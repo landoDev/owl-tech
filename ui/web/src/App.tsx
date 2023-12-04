a7d8695d30797f7d07a98725b2977c80d1e44cbe
landoDev <landonlewisturner@gmail.com>
2023-12-04 14:24:54 -0500

    implement year selector for reasonable load time

--- ui/web/src/App.tsx
+++ ui/web/src/App.tsx
@@ -1,7 +1,11 @@
 import React,  { useState, useEffect } from 'react';
 import axios from 'axios';
 import { LineChart } from '@mui/x-charts/LineChart';
-
+// break into component
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 // NOTE: these are in snake case because that is what comes back from the python BE at this time
 interface SectorLevel {
@@ -19,66 +23,74 @@ interface Stock {
   sector_level: SectorLevel
 }
 
-interface StockData {
-  xAxis: string[];
-  series: number[];
-}
-
 
 function App() {
-  const [stockData, setStocksData] = useState<{[index: string]: StockData} | {}>({})
-  const [selectedYear, setSelectedYear] = useState("2020")
+  const [stocks, setStocks] = useState<{[index: string]: Stock} | {}>({})
+  const [stockYear, setStockYear] = useState<string>("2015")
+  // should go in component, creates a list of years from now to 1999, when our stock data starts
+  const availableYears = Array.from({length: (new Date().getFullYear() - 1998)}, (_, i) => (new Date().getFullYear() - i).toString())
 
   useEffect(()=> {
     axios.get(`${process.env.REACT_APP_STOCKS_API_URL, 'http://127.0.0.1:8000'}/stocks`)
     .then(response => {
       // on response, set the stocks with their name as the key and a list of each stock price object associated
-      for (const [key, stockDetails] of Object.entries<Stock>(response.data)) {
-        // NOTE: 
-        setStocksData(prevStocks => ({
+      for (const [key, value] of Object.entries<Stock>(response.data)) {
+        // NOTE: in later iterations make a more scalable solution
+        // control for massive dataset with a selected year state; we assume format is YYYY-MM-DD in this app
+        value.stock_prices = value.stock_prices.filter(entry => entry.date.split("-")[0] === stockYear)
+        console.log(value)
+        setStocks(prevStocks => ({
           ...prevStocks,
-          [key]: {
-            "xAxis": stockDetails.stock_prices.map(stock => stock.date), scaleType: 'band',
-            "series": stockDetails.stock_prices.map(stock => stock.close_usd)
-          }
+          [key]: value
         }))
       }
     })
     .catch(error => {
       console.error(error);
     });
-  }, []);
+  }, [stockYear]);
 
-  console.log(stockData)
+  const handleChange = (event: SelectChangeEvent) => {
+    setStockYear(event.target.value);
+  };
+
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App" style={{margin:'1%'}}>
+      <header className="App-header" style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '2%'}}>
         <h1>OWL Stock Prices</h1>
+        <FormControl style={{marginRight: '35%', width: '10%'}}>
+        <InputLabel id="selectedYear">Year</InputLabel>
+        <Select
+          labelId="selectedYear"
+          id="stockYear-select"
+          value={stockYear}
+          label="Year"
+          onChange={handleChange}
+        >
+          {availableYears.map(year => <MenuItem value={year}>{year}</MenuItem>)}
+        </Select>
+      </FormControl>
       </header>
       <div className='App-container'>
         {/* this page is the home page */}
-        {/* call the all stocks endpoint on render and construct a line chart next to each asset's name  */}
-        {/* The asset name should be big and clickable */}
-        {/* an SPA page opens up with their Line chart, and the total cumulative returns and a table of the daily returns for their lifespan */}
-        {/* above this table is a date filter that the user can use to filter the date range and see the total cumulative returns and daily returns for that period */}
-        {/* <LineChart */}
-        {/* TODO: make me a component to keep App clean */}
-        {stockData && Object.entries(stockData).map(stock => {
+        {stocks && Object.entries(stocks).map(stock => {
           const [stockName, stockDetails] = stock
-          console.log(stockDetails)
-          // console.log('StockName', stockName, 'stockDetails',{} stockDetails.stock_prices.map(x => x.close_usd))
-          // console.log()
-          // this nested looping is not going to cut it
           return (
             <>
-            <div key={stockName}>{stockName}</div>
-            <LineChart
-              width={500}
-              height={300}
-              xAxis={[{data: stockDetails.xAxis.filter(stock => stock.split("-")[0] === selectedYear), scaleType: 'band'}]}
-              series={[{data: stockDetails.series[]}]}
+            {stockDetails.stock_prices.length ?
+            <>
+            <div>{stockName}</div>
+             <LineChart
+              width={1000}
+              height={500}
+              xAxis={[{data: stockDetails.stock_prices.map(stock => stock.date), scaleType: 'band'}]}
+              series={[{data: stockDetails.stock_prices.map(stock => stock.close_usd), label: 'USD'}]}
             />
+            </>
+            :
+            <div></div>
+          }
             </>
           )
         })
